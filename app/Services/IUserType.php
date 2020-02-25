e661a1bc908fe352e690ab71d76d1bc5da7b6f76
faisal-techverx <faisal.raza@gems.techverx.com>
2020-02-25 19:46:24 +0500

    Add user types to assign roles

---
+++ app/Services/IUserType.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace App\Services;
+
+/**
+ * Interface IUserType
+ *
+ * @package   App\Services
+ * @author    Faisal Raza <faisal.raza@gems.techverx.com>
+ * @copyright 2020 Techverx.com All rights reserved.
+ * @since     Feb 20, 2020
+ * @project   reloop
+ */
+interface IUserType
+{
+    const HOUSE_HOLD  = 1;
+    const ORGANIZATION = 2;
+    const DRIVER = 3;
+}
