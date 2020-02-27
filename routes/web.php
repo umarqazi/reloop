487e622a02d57286b2b98bc8a0b80474b8510ffc
faisal-techverx <faisal.raza@gems.techverx.com>
2020-02-27 14:47:41 +0500

    Home controller route issue fixed

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'Admin\HomeController@index')->name('home');
 Route::get('/activate-account/{id}/{token}', 'UserController@accountVerification');
 Route::get('/thankyou', 'PageController@thankyou')->name('thankyou');
 Route::resource('product', 'Admin\ProductController');
