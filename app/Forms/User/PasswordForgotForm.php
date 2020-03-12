eac29b434638e873fe0ae0ceb6940efe1fbaf1bc
faisal-techverx <faisal.raza@gems.techverx.com>
2020-03-12 17:22:59 +0500

    Api for forgot user password

---
+++ app/Forms/User/PasswordForgotForm.php
@@ -0,0 +1,42 @@
+<?php
+
+
+namespace App\Forms\User;
+use App\Forms\BaseForm;
+
+/**
+ * Class PasswordForgotForm
+ *
+ * @package   App\Forms\User
+ * @author    Faisal Raza <faisal.raza@gems.techverx.com>
+ * @copyright 2020 Techverx.com All rights reserved.
+ * @since     Mar 12, 2020
+ * @project   reloop
+ */
+class PasswordForgotForm extends BaseForm
+{
+
+    public $email;
+    public $token;
+    /**
+     * @inheritDoc
+     */
+    public function toArray()
+    {
+        return [
+            'email'     => $this->email,
+            'token'  => $this->token
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function rules()
+    {
+        return [
+            'email' => 'required|email',
+            'token' => 'required'
+        ];
+    }
+}
