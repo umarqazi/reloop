a4adeab9809ab96f60f26d20aa0d2cf76784c3d5
“HBilalSaqib” <bilal.saqib@techverx.com>
2020-04-24 19:00:31 +0500

    -fix

--- resources/views/includes/scripts.blade.php
+++ resources/views/includes/scripts.blade.php
@@ -6,19 +6,11 @@
 <script type="text/javascript" src="/assets/vendors/chartjs/chart.min.js"></script>
 <script type="text/javascript" src="/assets/js/plugins.js"></script>
 <script type="text/javascript" src="/assets/js/custom-script.js"></script>
+<script type="text/javascript" src="/assets/js/scripts/form-elements.js"></script>
 {{--
 <script type="text/javascript" src="/assets/js/scripts/dashboard-ecommerce.js"></script>
 --}}
 <script type="text/javascript" src="/assets/vendors/data-tables/js/jquery.dataTables.min.js"></script>
 <script type="text/javascript" src="/assets/js/scripts/data-tables.js"></script>
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-
-<script type="text/javascript" src="/assets/js/chart/bootstrap.min.js"></script>
-<script type="text/javascript" src="/assets/js/chart/owl.carousel.min.js"></script>
-<script type="text/javascript" src="/assets/js/chart/chart.js"></script>
-<script type="text/javascript" src="/assets/js/chart/custom.js"></script>{{--
-<script type="text/javascript" src="/assets/js/chart/jquery.min.js"></script>--}}
-<script type="text/javascript" src="/assets/js/chart/wow.min.js"></script>
-<script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
 <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
