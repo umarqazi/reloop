897d91a3b6260658518cdabffeb43a952c333a36
“HBilalSaqib” <bilal.saqib@techverx.com>
2020-04-28 11:02:17 +0500

    -order acceptances module added.

--- routes/web.php
+++ routes/web.php
@@ -42,6 +42,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::resource('collection-requests', 'Admin\CollectionRequestController');
     Route::resource('request-collections', 'Admin\RequestCollectionController');
     Route::resource('password-requests', 'Admin\PasswordResetController');
+    Route::resource('order-acceptances', 'Admin\OrderAcceptanceController');
 
     Route::get('get-cities', 'Admin\CityController@getCities')->name('getCities');
     Route::get('district-create/{city_id}', 'Admin\DistrictController@districtCreate')->name('districtCreate');
@@ -74,6 +75,7 @@ Route::group(['middleware' => ['auth']], function () {
     Route::get('/all-settings/export', 'Admin\SettingController@export')->name('settings.export');
     Route::get('/contactUs/export', 'Admin\ContactUsController@export')->name('contact-us.export');
     Route::get('/city/export', 'Admin\CityController@export')->name('cities.export');
+    Route::get('/orderAcceptances/export', 'Admin\OrderAcceptanceController@export')->name('orderAcceptances.export');
 
 
     //supervisor routes
