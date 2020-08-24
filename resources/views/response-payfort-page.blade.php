f9ec12c3cbd67a3415022c01c2221deb15d68661
faisal-techverx <faisal.raza@gems.techverx.com>
2020-08-24 20:14:45 +0500

    Add loader for payment pages Changes on payment confirmation page

---
+++ resources/views/response-payfort-page.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.app')
+@section('content')
+    <div class="main-loader-wrapper">
+        <img src="/assets/images/animation_200_ke9clp83.gif" class="loader-gif">
+        <p style="color: #fff;" class="loader-gif-text">Please wait, This may take some time. </p>
+    </div>
+
+    <script>
+        $(function() {
+            $('body').addClass('payment-confirm-page');
+        });
+    </script>
+@endsection
